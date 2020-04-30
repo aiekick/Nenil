3739491fe656e5ac07c2aaf3f8e08893b2ea2a30
Aiekick <aiekick@funparadigm.com>
2020-04-30 15:52:12 +0200

    7

--- cmake/3rdparty.cmake
+++ cmake/3rdparty.cmake
@@ -1,6 +1,5 @@
 if (CMAKE_SYSTEM_NAME STREQUAL Linux)
   find_package(X11 REQUIRED)
-
   if (NOT X11_Xi_FOUND)
     message(FATAL_ERROR "X11 Xi library is required")
   endif ()
--- cmake/ctools.cmake
+++ cmake/ctools.cmake
@@ -4,20 +4,18 @@ file(GLOB CTOOLS_HEADERS ${CTOOLS_INCLUDE_DIR}/*.h)
                  
 add_library(ctools STATIC ${CTOOLS_SOURCES} ${CTOOLS_HEADERS})
 
-add_definitions(-DGLAD)
+add_definitions(-DVOLK)
 add_definitions(-DIMGUI)
 
 include_directories(
     ${CTOOLS_INCLUDE_DIR}
-    ${OPENGL_INCLUDE_DIR}
+    ${Vulkan_INCLUDE_DIRS}
     ${GLFW_INCLUDE_DIR}
-    ${GLAD_INCLUDE_DIR}
 	${CMAKE_SOURCE_DIR}/3rdparty)
     
 target_link_libraries(ctools
-    ${OPENGL_LIBRARIES}
-    ${GLFW_LIBRARIES}
-    ${GLAD_LIBRARIES})
+    ${Vulkan_LIBRARIES}
+    ${GLFW_LIBRARIES})
     
 set_target_properties(ctools PROPERTIES LINKER_LANGUAGE CXX)
 set_target_properties(ctools PROPERTIES FOLDER 3rdparty)
